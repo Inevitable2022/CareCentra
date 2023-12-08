5d4eb268428771bfff7eb602da8b9ea43f552a66
Kapil Bhagat <107104808+Inevitable2022@users.noreply.github.com>
2023-12-09 02:46:57 +0530

    Add files via upload

---
+++ Code (2).py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install imutils
+
+
+# In[27]:
+
+
+# import the necessary packages
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
+import cv2
+
+# define the dictionary of digit segments so we can identify
+# each digit on the thermostat
+DIGITS_LOOKUP = {
+    (1, 1, 1, 1, 1, 1, 0): 0,
+    (0, 1, 1, 0, 0, 0, 0): 1,
+    (1, 1, 0, 1, 1, 0, 1): 2,
+    (1, 1, 1, 1, 0, 0, 1): 3,
+    (0, 1, 1, 0, 0, 1, 1): 4,
+    (1, 0, 1, 1, 0, 1, 1): 5,
+    (1, 0, 1, 1, 1, 1, 1): 6,
+    (1, 1, 1, 0, 0, 0, 0): 7,
+    (1, 1, 1, 1, 1, 1, 1): 8,
+    (1, 1, 1, 1, 0, 1, 1): 9
+}
+
+# load the example image
+image = cv2.imread("example_digits.png")
+
+# pre-process the image by resizing it, converting it to
+# grayscale, blurring it, and computing an edge map
+image = imutils.resize(image, height=500)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 50, 200, 255)
+
+# find contours in the edge map, then sort them by their
+# size in descending order
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+displayCnt = None
+
+# loop over the contours
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    if len(approx) == 4:
+        displayCnt = approx
+        break
+
+# extract the thermostat display, apply a perspective transform to it
+warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+output = four_point_transform(image, displayCnt.reshape(4, 2))
+
+# threshold the warped image, then apply a series of morphological operations to cleanup the thresholded image
+thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+# find contours in the thresholded image, then initialize the digit contours lists
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+digitCnts = []
+
+# loop over the digit area candidates
+for c in cnts:
+    (x, y, w, h) = cv2.boundingRect(c)
+    if w >= 15 and (h >= 30 and h <= 40):
+        digitCnts.append(c)
+
+# sort the contours from left-to-right, then initialize the actual digits themselves
+digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
+digits = []
+
+# loop over each of the digits
+for c in digitCnts:
+    (x, y, w, h) = cv2.boundingRect(c)
+    roi = thresh[y:y + h, x:x + w]
+
+    # compute the width and height of each of the 7 segments we are going to examine
+    (roiH, roiW) = roi.shape
+    (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+    dHC = int(roiH * 0.05)
+
+    # define the set of 7 segments
+    segments = [
+        ((0, 0), (w, dH)),	# top
+        ((0, 0), (dW, h // 2)),	# top-left
+        ((w - dW, 0), (w, h // 2)),	# top-right
+        ((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+        ((0, h // 2), (dW, h)),	# bottom-left
+        ((w - dW, h // 2), (w, h)),	# bottom-right
+        ((0, h - dH), (w, h))	# bottom
+    ]
+    on = [0] * len(segments)
+
+    # loop over the segments
+    for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+        segROI = roi[yA:yB, xA:xB]
+        total = cv2.countNonZero(segROI)
+        area = (xB - xA) * (yB - yA)
+        if total / float(area) > 0.5:
+            on[i] = 1
+
+    # lookup the digit and draw it on the image
+    on_tuple = tuple(on)
+    if on_tuple in DIGITS_LOOKUP:
+        digit = DIGITS_LOOKUP[on_tuple]
+        digits.append(digit)
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        cv2.putText(output, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+    else:
+        print(f"Unrecognized segment combination: {on_tuple}")
+
+
+# In[29]:
+
+
+# display the digits
+if digits:
+    if len(digits) == 3:
+        print(u"{}{}.{} \u00b0C".format(*digits))
+    else:
+        print("Digits:", " ".join(str(digit) for digit in digits))
+else:
+    print("No digits recognized")
+
+cv2.imshow("Input", image)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
